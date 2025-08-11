f50e1d8e8a8f06c76567fee71df4819320042537
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-08-12 02:36:38 +0600

    Add project files.

---
+++ StudentManagementApp1/Controllers/StudentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagementApp1.Models;
+
+namespace StudentManagementApp1.Controllers
+{
+    public class StudentController : Controller
+    {
+       
+        static List<Student> students= new List<Student>();
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            students.Add(student);
+            return RedirectToAction("Index");
+        }
+    }
+}
