6ed5ed9008922d42a146ff221f475225817f36c9
naimreza <78632945+naimrezap@users.noreply.github.com>
2025-08-12 13:18:18 +0600

    Edit Method are added.

--- StudentManagementApp1/Controllers/StudentController.cs
+++ StudentManagementApp1/Controllers/StudentController.cs
@@ -9,7 +9,8 @@ namespace StudentManagementApp1.Controllers
     {
         private readonly StudentDbContext _context;
 
-       public StudentController(StudentDbContext context)
+
+        public StudentController(StudentDbContext context)
         {
             _context = context;
         }
@@ -27,6 +28,33 @@ namespace StudentManagementApp1.Controllers
             _context.students.Add(student);
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var student = await _context.students.FindAsync(id);
+            if ((student == null)) return NotFound();
+            return View(student);
+
+
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Edit(int id, Student student)
+        {
+            if (id != student.ID) return NotFound();
+            if (ModelState.IsValid)
+            {
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+        private IActionResult NotFound()
+        {
+            return View();
         }
     }
 }
